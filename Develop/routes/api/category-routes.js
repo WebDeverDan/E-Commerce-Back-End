1654ae70c2a159781d36926eab0c1e22f053271d
Daniel Bradley <danielbvideo@gmail.com>
2021-09-07 14:01:22 -0400

    finalize category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -32,7 +32,9 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    const categoryData = await Category.create(req.body);
+    const categoryData = await Category.create({
+      category_name: req.body.category_name
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
@@ -43,10 +45,12 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   try {
   const categoryData = await Category.update({
-    where: {
+    category_name: req.body.category_name,},
+    { where: {
       id: req.params.id,
     },
-  });
+  }
+  );
 
   if (!categoryData) {
     res.status(404).json({ message: "No category found with that id" });
