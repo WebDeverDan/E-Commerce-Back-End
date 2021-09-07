62a22474be35080de8ba271a6020e30668fac9a0
Daniel Bradley <danielbvideo@gmail.com>
2021-09-07 16:35:10 -0400

    cleaned up formatting of category-routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 // import the model
 const { Category, Product, ProductTag, Tag } = require("../../models");
 
-
 router.get("/", async (req, res) => {
   try {
     const categoryData = await Category.findAll({
@@ -33,7 +32,7 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const categoryData = await Category.create({
-      category_name: req.body.category_name
+      category_name: req.body.category_name,
     });
     res.status(200).json(categoryData);
   } catch (err) {
@@ -44,26 +43,27 @@ router.post("/", async (req, res) => {
 // Daniel is not sure about this one
 router.put("/:id", async (req, res) => {
   try {
-  const categoryData = await Category.update({
-    category_name: req.body.category_name,},
-    { where: {
-      id: req.params.id,
-    },
-  }
-  );
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
 
-  if (!categoryData) {
-    res.status(404).json({ message: "No category found with that id" });
-    return;
-  }
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with that id" });
+      return;
+    }
 
-  res.status(200).json(categoryData);
+    res.status(200).json(categoryData);
   } catch (err) {
-  res.status(500).json(err);
+    res.status(500).json(err);
   }
 });
-
-
 
 router.delete("/:id", async (req, res) => {
   try {
