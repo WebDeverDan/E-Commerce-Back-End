a9619ca036bb53f5ec40cb6559ecbcb7714ea11d
Daniel Bradley <danielbvideo@gmail.com>
2021-09-07 16:35:30 -0400

    cleaned up formatting of index.js

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -6,7 +6,6 @@ const tagRoutes = require('./tag-routes');
 // this connects to the different routes the '/XXX' is for url ending
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
-// router.use('/product_tags', productTags);
 router.use('/tags', tagRoutes);
 
 module.exports = router;
